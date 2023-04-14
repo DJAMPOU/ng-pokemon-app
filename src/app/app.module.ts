8b321d40418365a4c91b70b491e7b5d0c95d7f87
Pedro <pierrewalter24@gmail.com>
2023-04-14 10:43:34 +0200

    Ajout des fichiers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    PokemonModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  canActivate(): boolean {
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
